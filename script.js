020443f6a9d39cb5c3e17f1eb655286be91c3bf0
filippo <tosifilippo@outlook.it>
2021-02-18 02:03:50 +0100

    created a game controller module with onclick functions

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 const gameBoard = (() => {
-    const boardArray = ["X","O","X","O","X","O","X","O","X"];
+    const boardArray = ["","","","","","","","",""];
     const boardGrid = document.createElement("div");
     boardGrid.id =  "board-grid";
     document.body.appendChild(boardGrid);
-    const topLeft = document.createElement("div");
+    const topLeft = document.createElement("button");
     topLeft.innerHTML = boardArray[0];
-    const topMiddle = document.createElement("div");
+    const topMiddle = document.createElement("button");
     topMiddle.innerHTML = boardArray[1];
-    const topRight = document.createElement("div");
+    const topRight = document.createElement("button");
     topRight.innerHTML = boardArray[2];
-    const centerLeft = document.createElement("div");
+    const centerLeft = document.createElement("button");
     centerLeft.innerHTML = boardArray[3];
-    const centerMiddle = document.createElement("div");
+    const centerMiddle = document.createElement("button");
     centerMiddle.innerHTML = boardArray[4];
-    const centerRight = document.createElement("div");
+    const centerRight = document.createElement("button");
     centerRight.innerHTML = boardArray[5];
-    const bottomLeft = document.createElement("div");
+    const bottomLeft = document.createElement("button");
     bottomLeft.innerHTML = boardArray[6];
-    const bottomMiddle = document.createElement("div");
+    const bottomMiddle = document.createElement("button");
     bottomMiddle.innerHTML = boardArray[7];
-    const bottomRight = document.createElement("div");
+    const bottomRight = document.createElement("button");
     bottomRight.innerHTML = boardArray[8];
     boardGrid.append(topLeft, topMiddle, topRight, centerLeft, centerMiddle, centerRight, bottomLeft, bottomMiddle, bottomRight);
+    return {
+        topLeft, topMiddle, topRight, centerLeft, centerMiddle, centerRight, bottomLeft, bottomMiddle, bottomRight
+    };
 })();
 
 const playerFactory = (mark) => {
     return { mark };
 };
 
-const playerOne = playerFactory("X");
-const playerTwo = playerFactory("O");
+const gameController = (() => {
+    const playerOne = playerFactory("X");
+    const playerTwo = playerFactory("O");
+    gameBoard.topLeft.onclick = function(){this.innerHTML = playerOne.mark};
+    gameBoard.topMiddle.onclick = function(){this.innerHTML = playerOne.mark};
+    gameBoard.topRight.onclick = function(){this.innerHTML = playerOne.mark};
+    gameBoard.centerLeft.onclick = function(){this.innerHTML = playerOne.mark};;
+    gameBoard.centerMiddle.onclick = function(){this.innerHTML = playerOne.mark};;
+    gameBoard.centerRight.onclick = function(){this.innerHTML = playerOne.mark};;
+    gameBoard.bottomLeft.onclick = function(){this.innerHTML = playerOne.mark};;
+    gameBoard.bottomMiddle.onclick = function(){this.innerHTML = playerOne.mark};;
+    gameBoard.bottomRight.onclick = function(){this.innerHTML = playerOne.mark};;
+})();
