9d97652f4fc7af879982fa23368c73181c5561b4
filippo <tosifilippo@outlook.it>
2021-02-20 06:50:17 +0100

    added logic for AI to make a random move

--- script.js
+++ script.js
@@ -12,46 +12,81 @@ const gameBoard = (() => {
     const playerTwo = playerFactory("O", "PLAYER TWO", "HUMAN");
     let gameOver;
     let tieGame;
-    // looks for winning combination in rows
+    // looks for winning combinations in rows
     function checkRows() {
-        if ((boardArray[0] == boardArray[1]) && (boardArray[1] == boardArray[2]) && (!boardArray[0] == "") ||
-        (boardArray[3] == boardArray[4]) && (boardArray[4] == boardArray[5]) && (!boardArray[3] == "") ||
-        (boardArray[6] == boardArray[7]) && (boardArray[7] == boardArray[8]) && (!boardArray[6] == "")) {
-            gameOver = true;
+        if (((boardArray[0] == boardArray[1]) && (boardArray[1] == boardArray[2]) && (!boardArray[0] == "") && (boardArray[0] == "X")) ||
+        ((boardArray[3] == boardArray[4]) && (boardArray[4] == boardArray[5]) && (!boardArray[3] == "") && (boardArray[3] == "X"))||
+        ((boardArray[6] == boardArray[7]) && (boardArray[7] == boardArray[8]) && (!boardArray[6] == "") && (boardArray[6] == "X"))) {
+            gameOver = "X";
+        };
+        if ((boardArray[0] == boardArray[1]) && (boardArray[1] == boardArray[2]) && (!boardArray[0] == "") && (boardArray[0] == "O") ||
+        (boardArray[3] == boardArray[4]) && (boardArray[4] == boardArray[5]) && (!boardArray[3] == "") && (boardArray[3] == "O") ||
+        (boardArray[6] == boardArray[7]) && (boardArray[7] == boardArray[8]) && (!boardArray[6] == "") && (boardArray[6] == "O")) {
+            gameOver = "O";
         };
     };
-    // looks for winning combination in columns
+    // looks for winning combinations in columns
     function checkColumns() {
-        if ((boardArray[0] == boardArray[3]) && (boardArray[3] == boardArray[6]) && (!boardArray[0] == "") ||
-        (boardArray[1] == boardArray[4]) && (boardArray[4] == boardArray[7]) && (!boardArray[1] == "") ||
-        (boardArray[2] == boardArray[5]) && (boardArray[5] == boardArray[8]) && (!boardArray[2] == "")) {
-            gameOver = true;
+        if ((boardArray[0] == boardArray[3]) && (boardArray[3] == boardArray[6]) && (!boardArray[0] == "") && (boardArray[0] == "X")||
+        (boardArray[1] == boardArray[4]) && (boardArray[4] == boardArray[7]) && (!boardArray[1] == "") && (boardArray[1] == "X") ||
+        (boardArray[2] == boardArray[5]) && (boardArray[5] == boardArray[8]) && (!boardArray[2] == "") && (boardArray[2] == "X")) {
+            gameOver = "X";
+        };
+        if ((boardArray[0] == boardArray[3]) && (boardArray[3] == boardArray[6]) && (!boardArray[0] == "") && (boardArray[0] == "O")||
+        (boardArray[1] == boardArray[4]) && (boardArray[4] == boardArray[7]) && (!boardArray[1] == "") && (boardArray[1] == "O")||
+        (boardArray[2] == boardArray[5]) && (boardArray[5] == boardArray[8]) && (!boardArray[2] == "") && (boardArray[2] == "O")) {
+            gameOver = "O";
         };
     };
-    // looks for winning combination in diagonals
+    // looks for winning combinations in diagonals
     function checkDiagonals() {
-        if ((boardArray[0] == boardArray[4]) && (boardArray[4] == boardArray[8]) && (!boardArray[0] == "") ||
-        (boardArray[2] == boardArray[4]) && (boardArray[4] == boardArray[6]) && (!boardArray[2] == "")) {
-            gameOver = true;
+        if ((boardArray[0] == boardArray[4]) && (boardArray[4] == boardArray[8]) && (!boardArray[0] == "") && (boardArray[0] == "X") ||
+        (boardArray[2] == boardArray[4]) && (boardArray[4] == boardArray[6]) && (!boardArray[2] == "") && (boardArray[2] == "X")) {
+            gameOver = "X";
+        };
+        if ((boardArray[0] == boardArray[4]) && (boardArray[4] == boardArray[8]) && (!boardArray[0] == "") && (boardArray[0] == "O") ||
+        (boardArray[2] == boardArray[4]) && (boardArray[4] == boardArray[6]) && (!boardArray[2] == "") && (boardArray[2] == "O")) {
+            gameOver = "O";
         };
     };
     // looks for tie
     function checkTie() {
-        if ((!gameOver == true) && (!boardArray.includes(""))) {
+        if ((gameOver == undefined) && (!boardArray.includes(""))) {
             tieGame = true;
         };
     };
-    // counts how many Xs and Os are on the board and determines whose turn it is. 
-    // it also looks for winner, does not run unless clicked field is empty.
     function setMark(field, index) {
+        // counts how many Xs and Os are on the board 
         let counts = {};
         boardArray.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
-        if (((counts.X === undefined) || (counts.X <= counts.O)) && (field.innerHTML === "")) {
-            field.innerHTML = playerOne.mark    
-            boardArray[index] = "X"
+        // for human players, determines turn depending on the number of Xs and Os and sets mark (cell must be empty)
+        if (playerTwo.species == "HUMAN") {
+            if (((counts.X === undefined) || (counts.X <= counts.O)) && (field.innerHTML === "")) {
+                field.innerHTML = playerOne.mark;
+                boardArray[index] = playerOne.mark;
+            } else if (field.innerHTML === "") {
+                field.innerHTML = playerTwo.mark;
+                boardArray[index] = playerTwo.mark;
+            };
+        // for AI player an array for all cells is created, first player mark is set (cell must be empty), a random 
+        // number is selected (from 0 to 8). If random cell is empty, second player mark is set, otherwise, new random
+        // is selected until empty cell is found.
         } else if (field.innerHTML === "") {
-            field.innerHTML = playerTwo.mark
-            boardArray[index] = "O"
+            let cellsArray = [topLeft, topMiddle, topRight, centerLeft, centerMiddle, centerRight, bottomLeft, bottomMiddle, bottomRight];
+            field.innerHTML = playerOne.mark;
+            boardArray[index] = "X";
+            function randomPlay() {
+            let random = Math.floor(Math.random() * cellsArray.length);
+            if (cellsArray[random].innerHTML == "") {
+                cellsArray[random].innerHTML = playerTwo.mark;
+                boardArray[random] = playerTwo.mark;
+            } else if (!boardArray.includes("")) {
+                declareWinner();
+            } else {
+                randomPlay();
+            };
+            };
+            randomPlay();
         };
         function declareWinner() {
             const display = document.getElementById("display");
@@ -60,12 +95,12 @@ const gameBoard = (() => {
             checkDiagonals();
             checkTie();
             // checks who won and disables field buttons
-            if ((gameOver == true) && (counts.X <= counts.O)) {
+            if (gameOver == "X") {
                 display.innerHTML = "GAME OVER, " + playerOne.name + " WINS!";
                 document.querySelectorAll('button.field-button').forEach(element => {
                     element.disabled = true;
                 });
-            } else if ((gameOver == true) && (counts.X > counts.O)) {
+            } else if (gameOver == "O") {
                 display.innerHTML = "GAME OVER, " + playerTwo.name + " WINS!";
                 document.querySelectorAll('button.field-button').forEach(element => {
                     element.disabled = true;
@@ -86,15 +121,22 @@ const gameBoard = (() => {
         boardArray = ["","","","","","","","",""];
         document.querySelectorAll('button.field-button').forEach(element => {
             element.innerHTML = "";
+            display.innerHTML = "";
             element.disabled = false;
-            gameOver = false;
-            tieGame = false;
+            gameOver = undefined;
+            tieGame = undefined;
         });
     };
-    // gets player names from input field
+    // gets player names and species from input field
     function getNames() {
         playerOne.name = firstText.value.toUpperCase();
         playerTwo.name = secondText.value.toUpperCase();
+        if (playerOne.name == "") {
+            playerOne.name = "PLAYER ONE"
+        }
+        if (playerTwo.name == "") {
+            playerTwo.name = "PLAYER TWO"
+        }
         if (form[3].checked) {
             playerTwo.species = "AI"
         }
@@ -116,7 +158,7 @@ const gameBoard = (() => {
     const firstText = document.getElementById("player1name");
     const secondText = document.getElementById("player2name");
     const submit = document.getElementById("submit");
-    const form = document.getElementById("form")
+    const form = document.getElementById("form");
     // onclick functions
     topLeft.onclick = function(){setMark(this, 0)};
     topMiddle.onclick = function(){setMark(this, 1)};
